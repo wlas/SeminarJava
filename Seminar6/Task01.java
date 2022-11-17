4981a641ce743d82ddec428ce183c7c76ce10c0b
SandrLite <mr.wlas@mail.ru>
2022-11-17 14:58:00 +0300

    Семинар 6 Дз 2

--- Seminar6/Task01.java
+++ Seminar6/Task01.java
@@ -1,18 +1,41 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
+ * 1. Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
+ * 2. Создать множество ноутбуков.
  */
 public class Task01 {
 
     public static void main(String[] args) {
 
-       NetBook nBook = new NetBook();
-       nBook.setModel("GR300");
-       nBook.setCpu("i3 2500");
-       nBook.setMemory(4096);
-       nBook.setDisplay(16.5);
-       nBook.setSerialNumber(865186535);
+       NetBook nBook0 = new NetBook();
+       nBook0.setModel("GR300");
+       nBook0.setCpu("i3 1500");
+       nBook0.setMemory(4096);
+       nBook0.setDisplay(16.5);
+       nBook0.setSerialNumber(865186535);
 
-       System.out.println(nBook);
+       NetBook nBook1 = new NetBook();
+       nBook1.setModel("GR301");
+       nBook1.setCpu("i5 2500");
+       nBook1.setMemory(4096);
+       nBook1.setDisplay(15);
+       nBook1.setSerialNumber(865186536);
+
+       NetBook nBook2 = new NetBook();
+       nBook2.setModel("GR303");
+       nBook2.setCpu("i7 3500");
+       nBook2.setMemory(8192);
+       nBook2.setDisplay(17);
+       nBook2.setSerialNumber(865186537);
+
+       Set<NetBook> booksSet = new HashSet<>();
+       booksSet.add(nBook0);
+       booksSet.add(nBook1);
+       booksSet.add(nBook2);
+
+       System.out.println(booksSet);
     }   
 }
